399700a130d82fdb330827067066990df88b7fe6
Austin Townsend <130945920+Pancakeshack@users.noreply.github.com>
2026-08-12 22:00:45 -0700

    add sample code

--- Sources/Octopus/Services/Llm/LocalLlmService.swift
+++ Sources/Octopus/Services/Llm/LocalLlmService.swift
@@ -1,7 +1,26 @@
 import FoundationModels
 
 struct LocalLlmService: LlmService {
+    private static let instructions = """
+        The input is a list of filenames in a folder. Your job is to provide new names for them to meet the
+        format of the Jellyfin media server. They must be a combination of the season number and the episode
+        number, for instance "s01e02.mkv". Keep the extension. Any files that are not media files you can ignore in the output.
+        Do not invent missing files or details, they must only be from the input.
+        """
+    private let model = SystemLanguageModel.default
+
     func generatePairs(files: [String]) async throws -> FilePairs {
-        fatalError("not yet implemented")
+        let session = LanguageModelSession(
+            model: model,
+            instructions: Self.instructions,
+        )
+
+        let prompt = files.joined(separator: ",")
+        let response = try await session.respond(
+            to: prompt,
+            generating: FilePairs.self
+        )
+
+        return response.content
     }
 }
